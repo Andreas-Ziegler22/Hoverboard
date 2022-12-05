904f692a7bfed7e31291ff1ec680eef9ad4a3fc7
MrBurp <andreass.ziegler@gmail.com>
2022-12-05 10:57:19 +0100

    increase are

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const container = document.getElementById("container");
 
 const colors = ["#e74c3c", "#8e44ad", "#3498db", "#e67e22", "#2ecc71"];
 
-const SQUARES = 600;
+const SQUARES = 1800;
 
 for (let i = 0; i < SQUARES; i++) {
   const square = document.createElement("div");
